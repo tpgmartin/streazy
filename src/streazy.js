a70d75f1deec5da07a468a26f1eb64517526f049
Tom Martin <tom@tpgmartin.com>
2016-07-20 16:32:26 +0100

    Fix linting issue

--- src/streazy.js
+++ src/streazy.js
@@ -9,7 +9,7 @@ export default class Streazy {
 
 function _createTail (tail) {
 
-   if (!tail.length || (tail[0] instanceof Array && !tail[0].length)) return function () {}
+  if (!tail.length || (tail[0] instanceof Array && !tail[0].length)) return function () {}
 
   return new Streazy(tail[0],tail.splice(1))
 
